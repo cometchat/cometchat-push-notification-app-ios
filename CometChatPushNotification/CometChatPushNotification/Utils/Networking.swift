7a7f6b9d182a72fe111c5b72bd48b574c9386100
Nabhodipta.CometChat <nabhodipta.garai@cometchat.com>
2024-06-22 01:57:18 +0530

    updated default users for login

---
+++ CometChatPushNotification/CometChatPushNotification/Utils/Networking.swift
@@ -0,0 +1,28 @@
+//
+//  Networking.swift
+//  CometChatPushNotification
+//
+//  Created by nabhodipta on 20/06/24.
+//
+
+import Foundation
+
+extension URLSession {
+  func fetchData<T: Decodable>(for url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+    self.dataTask(with: url) { (data, response, error) in
+      if let error = error {
+        completion(.failure(error))
+      }
+
+
+      if let data = data {
+        do {
+            let object = try JSONDecoder().decode(T.self, from: data)
+          completion(.success(object))
+        } catch let decoderError {
+          completion(.failure(decoderError))
+        }
+      }
+    }.resume()
+  }
+}
